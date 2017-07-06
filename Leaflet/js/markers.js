152ea2974394998473d5bcf908b8bb7caaa95039
Sheila Amalia <Sheila Amalia>
2017-07-06 08:27:50 +0700

    first commit

---
+++ Leaflet/js/markers.js
@@ -0,0 +1,18 @@
+var markers = [
+  {
+    "name":"Makassar",
+    "city":"Makassar, Indonesia",
+    "lat":-5.13342,
+    "lng":119.46115,
+  },{
+    "name":"Manado",
+    "city":"Manado, Indonesia",
+    "lat":1.52374,
+    "lng":124.84941,
+  },{
+	"name":"Kendari",
+    "city":"Kendari, Indonesia",
+    "lat":-3.9984597,
+    "lng":122.5129742,
+  }
+  ];
